6f571ec80dcf4542ddc9d981ab45633a917b215d
Scott Rush <scott.rush@gm.com>
2019-10-30 16:37:19 -0400

    Commit of draft initial FieldBlock and FieldGraph concept

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/FieldBlock.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+/**
+ * This is a data structure class used by the FieldGraph to represent the
+ * each block.
+ */
+public class FieldBlock {
+}
