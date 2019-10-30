b6a980861e17c12e029feb0462568be16bd3b0bb
Scott Rush <scott.rush@gm.com>
2019-10-30 16:37:19 -0400

    Commit of draft initial FieldBlock and FieldGraph concept

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/FieldGraph.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+/**
+ * This class is the graph used to define the Skystone playing field
+ * squares.  Creators add routes into the fieldgraph to be used in
+ * autonomous mode.
+ */
+public class FieldGraph {
+
+    public FieldGraph(int width,int height){
+
+    }
+}
