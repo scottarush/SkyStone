415996e549ddfb074c86ca29e2f7be7a16e62558
Scott Rush <scottarush@yahoo.com>
2019-11-03 10:57:44 -0500

    More updates to fieldgraph.  Still need to resolve how to get correct outgoing edge when route crosses tile more than once.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/Route.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/Route.java
@@ -1,7 +1,7 @@
 package org.firstinspires.ftc.teamcode.autonomous;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Iterator;
 
 /**
  * This the class for a route.
@@ -10,10 +10,11 @@ public class Route {
     private String mName;
     private FieldGraph mGraph;
 
-    FieldTile mStartTile = null;
-    FieldTile mEndTile = null;
     FieldTile mCurrentBuildTile = null;
 
+    private ArrayList<FieldTile> mRouteTileTransitions = new ArrayList<>();
+
+    private int mCurrentTransitionIndex = 0;
 
     public Route(String name,FieldGraph graph){
         mName = name;
@@ -32,23 +33,60 @@ public class Route {
         FieldTile tile = mGraph.getTile(tileNum);
         if (tile == null)
             return false;
-        mStartTile = tile;
+        mRouteTileTransitions.add(tile);
         tile.setStartOfRoute(this);
-        mCurrentBuildTile = mStartTile;
+        mCurrentBuildTile = tile;
         return true;
     }
 
-    public boolean addRouteTransition(int destTileNum){
+    public boolean addRouteTransition(int destTileNum,Action entryAction){
         FieldTile tile = mGraph.getTile(destTileNum);
         if (tile == null)
             return false;
-        boolean retcode = mCurrentBuildTile.addRouteTransition(this,tile);
+        boolean retcode = mCurrentBuildTile.addRouteTransition(this,tile,entryAction);
         if (retcode){
             mCurrentBuildTile = tile;
+            mRouteTileTransitions.add(tile);
         }
         return retcode;
     }
 
+    /**
+     * @return number of transitions on the route.
+     */
+    public int getNumTransitions(){
+        return mRouteTileTransitions.size();
+    }
+    /**
+     * @return the number of the current transition on this route
+     */
+    public int getCurrentTransitionNum(){
+        return mCurrentTransitionIndex;
+    }
+
+    /**
+     * Retrieves the transition index for the provided tile.  If there are multiple transitions
+     * on this tile, the incoming transition will be selected.
+     * @return index or -1 if tile is not on this route
+     */
+    private int getTransitionIndex(FieldTile tile){
+        // crawl the route to find the first occurence which have an outgoing edge or the endge tile
+        for(int index = 0;index < mRouteTileTransitions.size();index++){
+            FieldTile aTile = mRouteTileTransitions.get(index);
+            if (aTile == tile){
+                return index;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * Resets the route to run on the provided tile
+     * @return true if reset, false if tile was invalid.
+     */
+    public boolean resetRouteOnTile(FieldTile tile){
+        return getTransitionIndex(tile) >= 0;
+    }
     /**
      * Sets the ending tile of the route.  This route must have
      * been previously added to the tile or will return an error
@@ -59,9 +97,7 @@ public class Route {
         if (tile == null)
             return false;
         boolean retcod = tile.setEndOfRoute(this);
-        if (retcod){
-            mEndTile = tile;
-        }
+        mRouteTileTransitions.add(tile);
         return retcod;
     }
 
