7c164f645b9f18a7d616542b1849ffed82230e9c
Scott Rush <scottarush@yahoo.com>
2019-12-12 15:40:19 -0500

    Tweaked a comment

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/statemachines/gen_autosm_graph.bat
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/statemachines/gen_autosm_graph.bat
@@ -1,2 +1,2 @@
 java -jar Smc.jar -graph -glevel 2 SpeedBotAutoStateMachine.sm
-"C:\Program Files (x86)\Graphviz2.38\bin\dot.exe" -Tpng SpeedBotAutoStateMachine.dot > SpeedBotAutoStateMachine.png
+"C:\Program Files (x86)\Graphviz2.38\bin\dot.exe" -Tpng SpeedBotAutoStateMachine_sm.dot > SpeedBotAutoStateMachine.png
